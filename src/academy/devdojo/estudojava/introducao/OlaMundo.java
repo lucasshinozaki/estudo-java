e0caf9d8575c226a75bbade569dabde8cf081461
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-18 17:31:57 -0300

    Comentários em java

--- src/academy/devdojo/estudojava/introducao/OlaMundo.java
+++ src/academy/devdojo/estudojava/introducao/OlaMundo.java
@@ -1,6 +1,15 @@
 package academy.devdojo.estudojava.introducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaMundo {
+    // Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas
+     */
     public static void main (String[] args){
         System.out.println("Olá, Mundo");
     }
