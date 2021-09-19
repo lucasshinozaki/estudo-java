3604a7f33da296da4a36daba07f08ae83e5f580a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-19 13:28:05 -0300

    Aula04 Tipos Primiitivos 03 Casting

--- src/academy/devdojo/estudojava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/estudojava/introducao/Aula02TiposPrimitivos.java
@@ -3,12 +3,12 @@ package academy.devdojo.estudojava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 3200;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 87;
@@ -16,5 +16,6 @@ public class Aula02TiposPrimitivos {
         System.out.println("A idade é " + idade + " anos");
         System.out.println(verdadeiro);
         System.out.println(caractere);
+        System.out.println(salarioFloat);
     }
 }
