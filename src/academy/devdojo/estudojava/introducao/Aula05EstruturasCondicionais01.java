56a864896a925dfa0838adee8a9215cbd833157c
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-19 15:10:13 -0300

    Aula05 Estruturas Condicionais pt.02 - ELSE IF

--- src/academy/devdojo/estudojava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/estudojava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,16 +1,24 @@
 package academy.devdojo.estudojava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
         // !
         if(isAutorizadoComprarBebida){
             System.out.println("Autorizado a comprar bebida alcóolica");
+        }else {
+            System.out.println("Não Autorizado a comprar bebida alcólica");
         }
         if(!isAutorizadoComprarBebida){
             System.out.println("Não Autorizado a comprar bebida alcólica");
         }
+        boolean c = false;
+        if(c == true) {
+            System.out.println("Dentre de algo que nuvca deve ser feito");
+        }
+
         System.out.println("Fora do if");
+
     }
 }
