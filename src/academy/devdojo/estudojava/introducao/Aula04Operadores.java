9c7a303da18f7aa3af3a1f872997816e565f2d57
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-19 14:25:41 -0300

    Aula04 Operadores Atribuição [contador]

--- src/academy/devdojo/estudojava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/estudojava/introducao/Aula04Operadores.java
@@ -35,5 +35,26 @@ public class Aula04Operadores {
         float valorPlaystation = 5000F;
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /=2;
+        bonus %=2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        System.out.println(contador);
+        int contador2 = 0;
+        System.out.println(contador2++);
+        System.out.println(++contador2);
     }
 }
