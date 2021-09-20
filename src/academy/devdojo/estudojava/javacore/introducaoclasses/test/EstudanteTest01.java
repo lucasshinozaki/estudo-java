873e9ebb36d093deac3f479f4f9f3ead8a89ab6c
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-20 16:45:45 -0300

    Aula08 Introdução as classes pt 1

---
+++ src/academy/devdojo/estudojava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.estudojava.javacore.introducaoclasses.test;
+
+import academy.devdojo.estudojava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
