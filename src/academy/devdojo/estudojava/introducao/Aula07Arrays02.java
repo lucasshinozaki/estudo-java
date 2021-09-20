32df567bce32ff41a1044b4e31d616c6bbadeaca
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-20 13:57:25 -0300

    Aula06 Arrays pt 3

--- src/academy/devdojo/estudojava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/estudojava/introducao/Aula07Arrays02.java
@@ -2,14 +2,21 @@ package academy.devdojo.estudojava.introducao;
 
 public class Aula07Arrays02 {
     public static void main(String[] args) {
-        // byte, short, int, long, float, e double = 0
-        // char '\u000' ''
-        //boolean false
-        //String null
+        /**
+        byte, short, int, long, float, e double = 0
+        char \ u000 ''
+        boolean false
+        String null
+        */
 
-        int [] idades = new int[3];
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        nomes[3] = "Hinata";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
