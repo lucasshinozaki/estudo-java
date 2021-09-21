04b99965576002dd7fe829f5d50ef896f5d71e61
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-21 15:44:33 -0300

    Aula09 Orientação Objetos - Métodos pt 05 - Parâmetros tipo primitivo

--- src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -2,31 +2,39 @@ package academy.devdojo.estudojava.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
 
-    public void somaDoisNumeros(){
+    public void somaDoisNumeros() {
         System.out.println(10 + 10);
     }
 
-    public void subtrairDoisNumeros(){
+    public void subtrairDoisNumeros() {
         System.out.println(21 - 2);
     }
 
-    public void multiplaDoisNumeros(int num1, float num2){
+    public void multiplaDoisNumeros(int num1, float num2) {
         System.out.println(num1 * num2);
     }
 
-    public double divideDoisNumeros(double num1, double num2){
-        if(num2 == 0){
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             return 0;
         }
-        return num1/num2;
+        return num1 / num2;
     }
 
-    public void imprimeDivisãoDeDoisNumeros(double num1, double num2){
-        if(num2 == 0){
+    public void imprimeDivisãoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             System.out.println("Não existe divisão por zero");
             return;
-        }else {
-            System.out.println(num1/num2);
+        } else {
+            System.out.println(num1 / num2);
         }
+    }
+
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros: ");
+        System.out.println("Num1 " + num1);
+        System.out.println("Num2 " + num2);
     }
 }
