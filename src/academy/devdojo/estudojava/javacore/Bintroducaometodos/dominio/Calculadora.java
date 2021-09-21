ce8615f7c794a1bde61e3c63df0c4cc7babf3d8b
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-21 15:06:10 -0300

    Aula09 Orientação Objetos - Métodos pt 02

--- src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -9,4 +9,8 @@ public class Calculadora {
     public void subtrairDoisNumeros(){
         System.out.println(21 - 2);
     }
+
+    public void multiplaDoisNumeros(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
 }
---
+++ src/academy/devdojo/estudojava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.estudojava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.estudojava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplaDoisNumeros(2, 5);
+    }
+}
