2eafa64fb347dcaff7254242d7c8c72ace064a6f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-21 15:31:21 -0300

    Aula09 Orientação Objetos - Métodos pt 03 - Retorno pt 02

--- src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/estudojava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -20,4 +20,13 @@ public class Calculadora {
         }
         return num1/num2;
     }
+
+    public void imprimeDivisãoDeDoisNumeros(double num1, double num2){
+        if(num2 == 0){
+            System.out.println("Não existe divisão por zero");
+            return;
+        }else {
+            System.out.println(num1/num2);
+        }
+    }
 }
