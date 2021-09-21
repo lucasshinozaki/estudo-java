66afa0a9daee5c47522460cd16a9bc95d9f83a46
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-21 14:19:18 -0300

    Aula08 Introdução as classes Exerciocio Classes

---
+++ src/academy/devdojo/estudojava/javacore/Aintroducaoclasses/dominio/Professor.java
@@ -0,0 +1,7 @@
+package academy.devdojo.estudojava.javacore.Aintroducaoclasses.dominio;
+
+public class Professor {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/academy/devdojo/estudojava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.estudojava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.estudojava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Fusca";
+        carro1.ano = 1969;
+        carro1.modelo = "Sport";
+
+        carro2.nome = "Mustang";
+        carro2.ano = 1968;
+        carro2.modelo = "Gt-500";
+
+        System.out.println("Carro 1:");
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("---------------------------");
+
+        System.out.println("Carro 2:");
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+
+    }
+}
