a9c50fbdbd1292a86d96defb6cd444c9adfbfa5a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-22 15:14:30 -0300

    Aula12 Orientação Objetos - Construtores pt 02 - Sobrecarga

--- src/academy/devdojo/estudojava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/estudojava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,8 @@ import academy.devdojo.estudojava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyou", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyou", "TV", 12, "Ação", "Production 16");
         anime.imprime();
-        anime2.imprime();
+
     }
 }
