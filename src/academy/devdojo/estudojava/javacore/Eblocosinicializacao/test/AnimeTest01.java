7995189e12da1b6ed498343772066c235640e5e3
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-22 16:03:57 -0300

    Aula13 Orientação Objetos - Blocos de Inicialização

---
+++ src/academy/devdojo/estudojava/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.estudojava.javacore.Eblocosinicializacao.test;
+
+import academy.devdojo.estudojava.javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One Piece");
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+
+        }
+
+    }
+}
