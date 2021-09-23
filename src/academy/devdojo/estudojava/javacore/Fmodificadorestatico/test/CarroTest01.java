1d4ef085ce7cec424fc5fc04fd4a66f2720eec72
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-23 15:10:00 -0300

    Aula14 Orientação Objetos - Métodos estáticos

--- src/academy/devdojo/estudojava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/estudojava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,13 +4,11 @@ import academy.devdojo.estudojava.javacore.Fmodificadorestatico.domain.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
-
-        System.out.println(Carro.velocidadeLimite);
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite);
 
         c1.imprime();
         c2.imprime();
