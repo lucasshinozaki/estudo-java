627b0ad9646b2e8cedba8c6bea2fa50668633937
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-24 14:48:55 -0300

    Aula15 Orientação Objetos - Associação pt 04 - Associação Bidirecional

---
+++ src/academy/devdojo/estudojava/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,24 @@
+package academy.devdojo.estudojava.javacore.Gassociacao.test;
+
+import academy.devdojo.estudojava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.estudojava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pele");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("----Jogadores----");
+        jogador.imprime();
+        jogador2.imprime();
+
+        System.out.println("----Time----");
+        time.imprime();
+    }
+}
