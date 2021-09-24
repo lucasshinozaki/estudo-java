7d94d9d5ff248f9c67b4c234427e28baafe2e74d
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-24 17:04:43 -0300

    Aula17 Orientação Objetos - Herança pt 03 - Construtores

--- src/academy/devdojo/estudojava/javacore/Hherancao/dominio/Funcionario.java
+++ src/academy/devdojo/estudojava/javacore/Hherancao/dominio/Funcionario.java
@@ -3,6 +3,10 @@ package academy.devdojo.estudojava.javacore.Hherancao.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public Funcionario(String nome){
+        super(nome);
+    }
+
     public void imprime() {
         super.imprime();
         System.out.println(this.salario);
