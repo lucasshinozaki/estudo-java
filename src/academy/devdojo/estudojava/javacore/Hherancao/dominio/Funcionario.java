a72292f849f814e8887404a6b0c23bf97e8599b5
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-24 16:52:11 -0300

    Aula17 Orientação Objetos - Herança pt 03 - Protected

--- src/academy/devdojo/estudojava/javacore/Hherancao/dominio/Funcionario.java
+++ src/academy/devdojo/estudojava/javacore/Hherancao/dominio/Funcionario.java
@@ -1,16 +1,21 @@
 package academy.devdojo.estudojava.javacore.Hherancao.dominio;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     private double salario;
 
-    public void imprime(){
+    public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.nome + "recebi o salario de " + this.salario);
     }
 
     public double getSalario() {
         return salario;
     }
+
     public void setSalario(double salario) {
         this.salario = salario;
     }
---
+++ src/academy/devdojo/estudojava/javacore/Hherancao/test/Funcionario2.java
@@ -0,0 +1,11 @@
+package academy.devdojo.estudojava.javacore.Hherancao.test;
+
+import academy.devdojo.estudojava.javacore.Hherancao.dominio.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+
+    public void imprime(){
+        this.nome = "sasa";
+    }
+
+}
