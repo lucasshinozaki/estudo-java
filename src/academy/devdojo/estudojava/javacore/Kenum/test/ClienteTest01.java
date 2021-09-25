22d11119abb0b45cbe5bd4057c8f88b7e293367d
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-25 17:26:16 -0300

    Aula20 Orientação Objetos - Enumeração pt 01 - Introdução

---
+++ src/academy/devdojo/estudojava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.estudojava.javacore.Kenum.test;
+
+import academy.devdojo.estudojava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.estudojava.javacore.Kenum.dominio.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
+        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
+        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(cliente3);
+        System.out.println(cliente4);
+
+    }
+}
