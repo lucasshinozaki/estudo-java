ca543bdc5f2247447c220c288e16adea648e3a53
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-25 15:31:30 -0300

    Aula18 Orientação Objetos - Sobescrita do método toString

---
+++ src/academy/devdojo/estudojava/javacore/Isobreescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.estudojava.javacore.Isobreescrita.test;
+
+import academy.devdojo.estudojava.javacore.Isobreescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
