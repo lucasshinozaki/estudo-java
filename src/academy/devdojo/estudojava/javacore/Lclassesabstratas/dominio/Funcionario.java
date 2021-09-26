99a6745156d1ca2905b43f140cf1bf87f368d727
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-26 16:39:20 -0300

    Aula21 Orientação Objetos - Classes abstratas pt 02 - Métodos abstratos

--- src/academy/devdojo/estudojava/javacore/Lclassesabstratas/dominio/Funcionario.java
+++ src/academy/devdojo/estudojava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -7,6 +7,16 @@ public abstract class Funcionario {
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    public abstract void calculaBonus();
 
 }
