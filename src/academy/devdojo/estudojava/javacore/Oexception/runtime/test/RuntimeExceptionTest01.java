34ca0c761877cefd831a5dae4e6b5b9db49cb7fa
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-28 12:26:19 -0300

    Aula24 Excecções pt 03 - Exception

--- src/academy/devdojo/estudojava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/academy/devdojo/estudojava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.estudojava.javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         //Checked e Unchecked
-        Object object = null;
-        System.out.println(object.toString());
+       int[] nums = {1, 2};
+        System.out.println(nums[2]);
     }
 }
