c873c441343796b1ff45c4a346c097a091d58fa2
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-28 12:05:24 -0300

    Aula24 Excecções pt 02 - RuntimeException

---
+++ src/academy/devdojo/estudojava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.estudojava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked e Unchecked
+        Object object = null;
+        System.out.println(object.toString());
+    }
+}
