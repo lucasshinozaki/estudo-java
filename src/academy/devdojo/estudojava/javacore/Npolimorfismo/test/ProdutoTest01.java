3674f274d0b63e3a2ea88c1f9127a97794b1fe20
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-09-28 11:00:37 -0300

    Aula23 Orientação Objetos - Polimorfismo pt 03 - Parâmetros polimórficos

--- src/academy/devdojo/estudojava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/estudojava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.estudojava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.estudojava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.estudojava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.estudojava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.estudojava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10I7", 11000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Sansung 50\" ", 5000);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------------------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------------------------------------");
+        CalculadoraImposto.calcularImposto(tv);
+
     }
 }
