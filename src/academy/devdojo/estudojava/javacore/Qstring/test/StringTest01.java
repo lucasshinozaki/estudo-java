43952d73de5210849a54d4267f5b3cc2530103dc
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-07 14:00:15 -0300

    Aula26 Classes Ultilitárias - String pt 01

---
+++ src/academy/devdojo/estudojava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.estudojava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        //toda String em java é imutavel
+        String nome = "Lucas"; //String constant pool
+        String nome2 = "Lucas";
+
+        nome = nome.concat(" Shinozoaki");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+
+        // 1 - variavel de referência
+        // 2 - objeto do tipo string
+        // 3 - uma string na pool
+        String nome3 = new String("Lucas"); //pasando por referencia do pool de string, dentro do heap, fora do pool
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());//comparando dentro do pool de string .intern()
+    }
+}
