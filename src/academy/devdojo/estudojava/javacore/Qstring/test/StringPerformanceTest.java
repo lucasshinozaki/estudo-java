455e1d96a43f51e7ca2351f1203bc9f2e02f3149
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-07 15:15:44 -0300

    Aula26 Classes Ultilitárias - String pt 03 - Desempenho

---
+++ src/academy/devdojo/estudojava/javacore/Qstring/test/StringPerformanceTest.java
@@ -0,0 +1,52 @@
+package academy.devdojo.estudojava.javacore.Qstring.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(10_000);
+        long fim = System.currentTimeMillis();
+
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(100_000);
+        fim = System.currentTimeMillis();
+
+        System.out.println("Tempo gasto para StringBuilder " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(100_000);
+        fim = System.currentTimeMillis();
+
+        System.out.println("Tempo gasto para StringBuffer " + (fim - inicio) + "ms");
+    }
+
+    //micro-desempenho eleva a complexidade do código
+
+    // Tem um gasto maior
+    private static void concatString(int tamanho){
+        String texto = "";
+
+        for (int i = 0; i < tamanho; i++) {
+            texto += i; // 0, 01, 012, 013
+        }
+    }
+
+    // Utilizada para diminuir o tempo
+    private static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i); // 0, 01, 012, 013
+        }
+    }
+
+    // Criada para utilizar múltiplas threads, usa uma thread por vez
+    private static void concatStringBuffer(int tamanho){
+        StringBuffer sb = new StringBuffer(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i); // 0, 01, 012, 013
+        }
+    }
+}
