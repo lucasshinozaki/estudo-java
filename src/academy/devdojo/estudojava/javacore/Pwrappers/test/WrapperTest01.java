c49e0d7de3d7eb5cb857427bfb787f0155bb10bb
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-07 10:36:27 -0300

    Aula25 Classes Ultilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/estudojava/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.estudojava.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        //Regra de Polimorfismo é aplicado, todos são objetos
+        //Passar valores por referência
+        //Trabalhar com estrutras de dados(que não trabalha com tipos primitivos)
+        //Trabalhar com threads
+        //Bons modos usar tipo primitivo sempre que possível
+        //Ex List<Integer> list;
+
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+    }
+}
