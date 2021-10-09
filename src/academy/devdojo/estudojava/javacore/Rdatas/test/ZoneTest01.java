6507eee57f8c736c406dd4d4cf6557e1b39a94d4
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-09 16:49:00 -0300

    Aula28 Classes Ultilitárias - ZonedDateTime, ZoneId, OffsetDateTime

---
+++ src/academy/devdojo/estudojava/javacore/Rdatas/test/ZoneTest01.java
@@ -0,0 +1,47 @@
+package academy.devdojo.estudojava.javacore.Rdatas.test;
+
+import java.time.*;
+import java.time.chrono.JapaneseDate;
+import java.util.Map;
+
+public class ZoneTest01 {
+    public static void main(String[] args) {
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        System.out.println(shortIds);
+        System.out.println(ZoneId.systemDefault());
+
+        ZoneId tokyoZone = ZoneId.of("Asia/Tokyo");
+        System.out.println(tokyoZone);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        ZonedDateTime zonedDateTime = now.atZone(tokyoZone);
+        System.out.println(zonedDateTime);
+
+        Instant nowInstant = Instant.now();
+        System.out.println(nowInstant); //Zulu-Time
+        ZonedDateTime zonedDateTime2 = nowInstant.atZone(tokyoZone);
+        System.out.println(zonedDateTime2);
+
+        System.out.println(ZoneOffset.MIN);
+        System.out.println(ZoneOffset.MAX);
+
+        ZoneOffset offsetManaus = ZoneOffset.of("-04:00");
+        OffsetDateTime offsetDateTime = now.atOffset(offsetManaus);
+        System.out.println(offsetDateTime);
+
+        OffsetDateTime offsetDateTime3 = OffsetDateTime.of(now, offsetManaus);
+        System.out.println(offsetDateTime3);
+
+        OffsetDateTime offsetDateTime4 = nowInstant.atOffset(offsetManaus);
+        System.out.println(offsetDateTime4);
+
+        JapaneseDate japanesDate = JapaneseDate.from(LocalDate.now());
+        System.out.println(japanesDate);
+
+        LocalDate meijiEraLocalDate = LocalDate.of(1900,2,1);
+        JapaneseDate meijiEra = JapaneseDate.from(meijiEraLocalDate);
+        System.out.println(meijiEra);
+    }
+}
