f8b606279dd7f07c12dd48eff876de592438f811
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-10 14:58:48 -0300

    Aula30 Classes Ultilitárias - Regex pt 02 - Pattern e Matcher - Range

---
+++ src/academy/devdojo/estudojava/javacore/Uragex/test/PatternMatcherTest03.java
@@ -0,0 +1,28 @@
+package academy.devdojo.estudojava.javacore.Uragex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        //Texto com numeros hexadecimais válidos
+        //Meta caracteres
+        // []
+
+//        String regex = "[a-zA-C]";
+        String regex = "0[xX][0-9a-fA-F]";
+        String texto2 = "abaaba";
+        String texto = "12 0x 0X 0xFFABC 0x109 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+
+        System.out.println("texto:  "+texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posições encontradas");
+
+        while(matcher.find()){
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+    }
+}
