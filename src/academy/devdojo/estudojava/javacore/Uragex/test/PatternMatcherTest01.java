b93955374696fdd9d3ca739a62f04ff9b4a72fe6
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-10 14:39:56 -0300

    Aula30 Classes Ultilitárias - Regex pt 01 - Pattern e Matcher

---
+++ src/academy/devdojo/estudojava/javacore/Uragex/test/PatternMatcherTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.estudojava.javacore.Uragex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest01 {
+    public static void main(String[] args) {
+        //Utiliza simbolos que encontra padrões no texto
+        //Serve para validação. Ex:email válido
+
+        String regex = "aba";
+        String texto2 = "abaaba";
+        String texto = "abababa";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+
+        System.out.println("texto:  "+texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex "+regex);
+        System.out.println("Posições encontradas");
+
+        while(matcher.find()){
+            System.out.print(matcher.start()+" ");
+        }
+    }
+}
