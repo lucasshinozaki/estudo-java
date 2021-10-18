b43b6da369eb121ba02e9ab81fc3fe5acf9cca2a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-18 15:49:04 -0300

    Aula39 Lambdas pt 01 - Predicate

--- src/academy/devdojo/estudojava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/estudojava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,11 @@ package academy.devdojo.estudojava.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.estudojava.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anonimas, funções(Sem atrelamento de classes), objetivo: concisão
     boolean test(Car car);
+    //(parametro) -> <expressão>
+    //(Car car)   -> car.getColor().equals("greem") Tem que retornar um boolean
+
 }
