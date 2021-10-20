38e0b92e35fff7c84a9989923e72e9843ab0a135
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-20 16:01:49 -0300

    Aula40 Method Reference pt 01 - Refência a métodos estáticos

--- src/academy/devdojo/estudojava/javacore/ZZClambdas/test/LambdaTest02.java
+++ src/academy/devdojo/estudojava/javacore/ZZClambdas/test/LambdaTest02.java
@@ -7,8 +7,8 @@ import java.util.function.Function;
 public class LambdaTest02 {
     public static void main(String[] args) {
         List<String> strings = List.of("Natsu", "Allucard");
-        List<Integer> integers = map(strings, s -> s.length());
-        List<String> upper = map(strings, s -> s.toUpperCase());
+        List<Integer> integers = map(strings, String::length);
+        List<String> upper = map(strings, String::toUpperCase);
         System.out.println(integers);
         System.out.println(upper);
     }
