79a9a88cfe642827054f30efb8e22d0397c0a2f5
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-21 14:09:00 -0300

    Aula41 Optional pt 02

---
+++ src/academy/devdojo/estudojava/javacore/ZZDoptional/test/OptionalTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.estudojava.javacore.ZZDoptional.test;
+
+import academy.devdojo.estudojava.javacore.ZZDoptional.dominio.Manga;
+import academy.devdojo.estudojava.javacore.ZZDoptional.repositorio.MangaRepository;
+
+import java.util.Optional;
+
+public class OptionalTest02 {
+    public static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Boku no Hero");
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2)
+                .orElseThrow(IllegalArgumentException::new);
+
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.findByTitle("Drifters")
+                .orElseGet(() -> new Manga(3, "Drifters", 20));
+        System.out.println(newManga);
+    }
+}
