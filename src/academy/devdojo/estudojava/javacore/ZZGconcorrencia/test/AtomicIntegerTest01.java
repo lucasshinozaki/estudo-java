9124d4de4fa382ceef292943cada323069ad0bc8
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-25 10:35:57 -0300

    Aula44 Concorrência pt 02 - Lock and ReentrantLock

--- src/academy/devdojo/estudojava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
+++ src/academy/devdojo/estudojava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
@@ -1,14 +1,22 @@
 package academy.devdojo.estudojava.javacore.ZZGconcorrencia.test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Counter {
     private int count;
     private AtomicInteger atomicInteger = new AtomicInteger();
+    private Lock lock = new ReentrantLock(true);
 
     void increment(){
-        count++;
-        atomicInteger.incrementAndGet();
+        lock.lock();
+        try {
+            count++;
+            atomicInteger.incrementAndGet();
+        }finally {
+            lock.unlock();
+        }
     }
 
     public AtomicInteger getAtomicInteger() {
