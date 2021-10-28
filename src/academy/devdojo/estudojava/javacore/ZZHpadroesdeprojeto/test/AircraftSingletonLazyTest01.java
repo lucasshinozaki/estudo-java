e8aaf77a7b3ad5936e3cc182993bc7e9d5773d85
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-28 16:48:18 -0300

    Aula45 Padrões de Projeto pt 04 - Singleton pt 02 - Lazy initialization

---
+++ src/academy/devdojo/estudojava/javacore/ZZHpadroesdeprojeto/test/AircraftSingletonLazyTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.estudojava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.estudojava.javacore.ZZHpadroesdeprojeto.dominio.AircraftSingletonLazy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class AircraftSingletonLazyTest01 {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        bookSeat("1A");
+        bookSeat("1A");
+
+        System.out.println(AircraftSingletonLazy.getINSTACE());
+        System.out.println(AircraftSingletonLazy.getINSTACE());
+
+        // Não fazer assim.
+        Constructor<AircraftSingletonLazy> constructor = AircraftSingletonLazy.class.getDeclaredConstructor(String.class);
+        constructor.setAccessible(true);
+        AircraftSingletonLazy aircraftSingletonLazy = constructor.newInstance("787-900");
+        System.out.println(aircraftSingletonLazy);
+    }
+
+    private static void bookSeat(String seat){
+        System.out.println(AircraftSingletonLazy.getINSTACE());
+        AircraftSingletonLazy aircraft = AircraftSingletonLazy.getINSTACE();
+        System.out.println(aircraft.bookSeat(seat));
+    }
+
+}
