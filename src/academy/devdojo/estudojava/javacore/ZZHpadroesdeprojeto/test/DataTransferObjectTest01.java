1797522d6e0960b20da1d4609deb8c45b2dabe44
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-28 17:06:37 -0300

    Aula45 Padrões de Projeto pt 06 - Data Transfer Object

---
+++ src/academy/devdojo/estudojava/javacore/ZZHpadroesdeprojeto/test/DataTransferObjectTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.estudojava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.estudojava.javacore.ZZHpadroesdeprojeto.dominio.*;
+
+public class DataTransferObjectTest01 {
+    public static void main(String[] args) {
+        Aircraft aircraft = new Aircraft("777");
+        Country country = Country.BRAZIL;
+        Currency currency = CurrencyFactory.newCurrency(country);
+        Person person = Person.PersonBuilder
+                .builder()
+                .firstName("Lucas")
+                .lastName("Shinozaki")
+                .build();
+
+        ReportDto reportDto = ReportDto.ReportDtoBuilder.builder()
+                .aircraftName(aircraft.getName())
+                .country(country)
+                .currency(currency)
+                .personName(person.getFirstName())
+                .build();
+
+        System.out.println(reportDto);
+    }
+}
