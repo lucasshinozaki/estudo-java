384d4deb932f6026e656c955008fd1adc0dedbe6
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-30 14:10:06 -0300

    Aula46 JDBC pt 05 - Adicionando dependências e conectando com o banco

---
+++ src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package academy.devdojo.estudojava.javacore.ZZIjdbc.test;
+
+import academy.devdojo.estudojava.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
