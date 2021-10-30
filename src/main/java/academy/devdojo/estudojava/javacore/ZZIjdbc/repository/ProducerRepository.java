7b6a51bc7851ecfac3c64f49831a1a3c5106b535
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-30 15:18:58 -0300

    Aula46 JDBC pt 08 - Deletanto dados com Statement

--- src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -14,12 +14,25 @@ public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
 
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Inserted producer '{}' in database, rows affected '{}' ", producer.getName(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(int id) {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = '%d');".formatted(id);
 
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            log.info("Inserted producer in database, rows affected '{}' ", rowsAffected);
+            log.info("Deleted producer '{}' in database, rows affected '{}' ",id, rowsAffected);
         } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
             e.printStackTrace();
         }
     }
--- src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,18 +1,13 @@
 package academy.devdojo.estudojava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.estudojava.javacore.ZZIjdbc.dominio.Producer;
-import academy.devdojo.estudojava.javacore.ZZIjdbc.repository.ProducerRepository;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
-//        log.info("INFO");
-//        log.debug("debug");
-//        log.warn("warn");
-//        log.error("error");
-//        log.trace("trace");
+//        ProducerService.save(producer);
+//        ProducerService.delete(4);
     }
 }
