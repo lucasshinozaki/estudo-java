0938129f631f3246368f4de180d2445d5176d7dc
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-30 14:58:34 -0300

    Aula46 JDBC pt 07 - Lombock e Log4J2

--- src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,11 +2,14 @@ package academy.devdojo.estudojava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.estudojava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.estudojava.javacore.ZZIjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
@@ -15,8 +18,7 @@ public class ProducerRepository {
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
-
+            log.info("Inserted producer in database, rows affected '{}' ", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
