9ab41e38d16153f7da38992afc6bcdf1f9fcfd6f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2021-10-31 16:31:23 -0300

    Aula46 JDBC pt 12 - ResultSetMetaData

--- src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/estudojava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -4,8 +4,6 @@ import academy.devdojo.estudojava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.estudojava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.List;
-
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
@@ -15,8 +13,9 @@ public class ConnectionFactoryTest01 {
 //        ProducerService.delete(4);
 //        ProducerService.update(producerToUpdate);
 
-        List<Producer> producers = ProducerService.findAll();
+//        List<Producer> producers = ProducerService.findAll();
 //        List<Producer> producers = ProducerService.findByName("Mad");
-        log.info("Producers found '{}'", producers);
+//        log.info("Producers found '{}'", producers);
+        ProducerService.showProducerMetaData();
     }
 }
